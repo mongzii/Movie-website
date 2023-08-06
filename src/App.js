dd0c653e7ef514e4f59cc4dd74cc3954d43679e9
moonjiyoung <umarg228@naver.com>
2023-08-06 17:54:56 +0900

    feat: 카테고리 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 import Documentary from "./category/Documentary";
+import Action from "./category/Action";
+import Music from "./category/Music";
+import Biography from "./category/Biography";
+import Drama from "./category/Drama";
+import Mystery from "./category/Mystery";
 
 // import Movie from "./components/Movie";
 
@@ -12,9 +17,13 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/movie" element={<Movie />} /> */}
           <Route path="/movie/:id" element={<Detail />} />
           <Route path="/movie/documentary" element={<Documentary />} />
+          <Route path="/movie/action" element={<Action />} />
+          <Route path="/movie/biography" element={<Biography />} />
+          <Route path="/movie/drama" element={<Drama />} />
+          <Route path="/movie/music" element={<Music />} />
+          <Route path="/movie/mystery" element={<Mystery />} />
         </Routes>
       </BrowserRouter>
     </>

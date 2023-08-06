8c089c45706730053b02257acc83413f2b9ac83a
moonjiyoung <umarg228@naver.com>
2023-08-06 18:55:04 +0900

    feat: summary 길이설정

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
 const TotalMained = styled.div`
-  border: 10px solid red;
-  margin: 5px;
+  /* border: 10px solid red; */
+  width: 88%;
+  margin: 20px;
   display: flex;
   flex-direction: row;
 `;
 const PartMained = styled.div`
-  border: 5px solid blue;
+  /* border: 5px solid blue; */
   margin: 10px;
+  padding: 7px;
   display: flex;
   flex-direction: column;
 `;
@@ -25,7 +27,9 @@ function Movie({ id, coverImg, title, summary, genres }) {
             {/* 제목 클릭시 Detail컴포넌트연결 */}
           </h2>
 
-          <p>{summary}</p>
+          <p>
+            {summary.length > 250 ? `${summary.slice(0, 250)}......` : summary}
+          </p>
           <ul>
             {genres.map(item => (
               <li key={item}>{item}</li>

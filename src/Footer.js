0bf90e201e4ff7389b835f4b4957f30ed4ad7bc3
moonjiyoung <umarg228@naver.com>
2023-08-06 18:56:03 +0900

    design: 길이 수정

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,30 @@
 import { styled } from "styled-components";
 
 const Aaa = styled.div`
-  border: 1px solid black;
-  background-color: skyblue;
+  /* border: 1px solid black; */
+  border-radius: 10px;
+  background-color: black;
+  color: white;
+  width: 99%;
   height: 7rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > h4 {
+    padding-right: 15px;
+  }
+  > h3 {
+    padding-right: 15px;
+  }
 `;
 
 function Footer() {
   return (
     <div>
-      <Aaa>By mongzii</Aaa>
+      <Aaa>
+        <h4>Copy right 2023.</h4>
+        <h3>by mongzii</h3>
+      </Aaa>
     </div>
   );
 }

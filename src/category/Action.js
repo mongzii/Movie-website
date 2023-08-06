8c7f790c116f8d805c3b01be0e47b0238a58355b
moonjiyoung <umarg228@naver.com>
2023-08-06 17:52:45 +0900

    feat: action 카테고리 생성

---
+++ src/category/Action.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+// import Movie from "../components/Movie";
+
+function Action() {
+  //   let result = [];
+  const [loading, setLoading] = useState(true);
+  const [newArr, setNewArr] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.9&sort_by=year"
+      )
+      .then(res => {
+        // console.log(res.data.data.movies);
+        let newMovies = res.data.data.movies;
+        for (let i = 0; i < newMovies.length; i++) {
+          for (let j = 0; j < newMovies[i].genres.length; j++) {
+            let filtered = newMovies.filter(movie =>
+              movie.genres.includes("Action")
+            );
+            // console.log(filtered);
+            setNewArr(filtered);
+            setLoading(false);
+
+            // if (newMovies[i].genres[j] === "Documentary") {
+            //   console.log(res.data.data.movies[i]);
+            //   setNewArr(res.data.data.movies[i]);
+            //   console.log(newArr);
+            //   result.push(newMovies[i]);
+            // }
+          }
+
+          //   console.log(newArr);
+          //   console.log(res.data.data.movies);
+        }
+      });
+  }, []);
+  //   console.log(newArr);
+  //   console.log(result[0]);
+
+  return (
+    <>
+      {/* <p>documentary모은거다</p> */}
+      {/* {console.log(newArr[0].title)} */}
+      {/* <p>{console.log(result)}</p> */}
+      {/* <p>{console.log(result[0])}</p> */}
+      {/* {console.log(result[0])} */}
+      {loading ? (
+        <h1>Loading....</h1>
+      ) : (
+        <div>
+          {newArr.map(el => (
+            <p key={el.id}>{el.title}</p>
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
+export default Action;
